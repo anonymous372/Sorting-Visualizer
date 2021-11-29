0730e2f7a18389575a33b96dee157fa02f658a45
Sourabh Shukla <82205268+anonymous372@users.noreply.github.com>
2021-11-29 16:30:42 +0530

    Add files via upload

--- Sorting Visualizer/sort_vis.js
+++ Sorting Visualizer/sort_vis.js
@@ -1,17 +1,85 @@
-var nums = 10;
+var nums = 50;
+var delay = 50;
 
 var grid = document.getElementById("container")
 
 var arNum = []
 
-for(var i=0;i<nums;i++){
-    var val = Math.trunc(Math.random()*100);
+for (var i = 0; i < nums; i++) {
+    var val = Math.trunc(Math.random() * 100);
     arNum.push(val)
-    
+
     div = document.createElement("div")
     div.classList.add("nums")
-    div.style.height = val+"%"
+    div.style.height = val + "%"
+    div.style.width = 60/nums + "%"
 
     grid.append(div)
-
 }
+
+// The delay/sleep function
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function swap(i,j){
+    [arNum[i], arNum[j]] = [arNum[j], arNum[i]]
+}
+
+// Simple bubble sort
+async function bubbleSort() {
+    for (var i = 0; i < nums; i++){
+        for (var j = i+1; j < nums; j++) {
+            show_cur(i,j)
+            if (arNum[i] > arNum[j])
+                swap(i,j)    
+            
+            update();
+            await sleep(delay);
+            show_cur(i,j);
+        }
+    }
+}
+
+async function selectionSort() {
+    for (var i = 0; i < nums; i++){
+        var curMin = i;
+        for (var j = i+1; j < nums; j++) {
+            show_cur(i,j)
+            if (arNum[curMin] > arNum[j])
+                curMin = j
+            
+            await sleep(delay);
+            show_cur(i,j);
+        }
+        swap(i,curMin);
+        update();
+    }
+}
+
+async function insertionSort(){
+}
+
+async function mergeSort(){
+}
+
+async function quickSort(){
+}
+
+
+function show_cur(i,j){
+    var numbs = document.querySelectorAll(".nums");
+    
+    numbs[i].classList.toggle("cur_num")
+    numbs[j].classList.toggle("cur_num")
+}
+
+function update() {
+    var numbs = document.querySelectorAll(".nums");
+    for (var i = 0; i < nums; i++) {
+        numbs[i].style.height = arNum[i] + "%";
+    }
+}
+
+bubbleSort();
+// selectionSort();
